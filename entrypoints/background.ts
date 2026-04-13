b63ccfc40dad013b4b7557665e9c0a8da6d5e24e
FC <drslen9@gmail.com>
2026-04-12 18:46:03 -0700

    test: add sender verification and alarm race guard test coverage

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -237,9 +237,16 @@ export default defineBackground(() => {
     await recoverFromMissedAlarm();
   });
 
-  browser.runtime.onMessage.addListener((message) => handleMessage(message as MessageAction));
+  browser.runtime.onMessage.addListener((message, sender) => {
+    if (sender.id !== undefined && sender.id !== browser.runtime.id) {
+      return false;
+    }
+    return handleMessage(message as MessageAction);
+  });
 
-  browser.alarms.onAlarm.addListener(async (alarm) => {
+  let alarmHandling = false;
+
+  const handleAlarm = async (alarm: browser.alarms.Alarm): Promise<void> => {
     if (alarm.name === ALARM_BADGE_REFRESH) {
       await refreshBadge();
       return;
@@ -291,5 +298,15 @@ export default defineBackground(() => {
     }
 
     await refreshBadge();
+  };
+
+  browser.alarms.onAlarm.addListener(async (alarm) => {
+    if (alarmHandling) return;
+    alarmHandling = true;
+    try {
+      await handleAlarm(alarm);
+    } finally {
+      alarmHandling = false;
+    }
   });
 });
